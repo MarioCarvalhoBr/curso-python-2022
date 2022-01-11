258a3c86b28a397d8a8e8c6c8b8c6977b0c1948a
Mário Carvalho <mariodearaujocarvalho@gmail.com>
2022-01-10 21:03:37 -0300

    Cadastro de pessoas usando for e listas

--- verifica_frutas.py
+++ verifica_frutas.py
@@ -1,5 +1,5 @@
 frutas = ["maça", "banana", "uva"]
 if "kiwi" in frutas:
-  print("Sim, kiwi está dentro da lista de frutas")
+    print("Sim, kiwi está dentro da lista de frutas")
 else:
     print("kiwi não está dentro da lista")
